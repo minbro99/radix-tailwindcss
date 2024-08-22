2d2f4e106c41d1ca307daf41809c601c2e2e0214
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-22 18:41:01 +0900

    Feat: 새로운 파일이 추가된 components 폴더 업로드
    새로운 파일이 추가된 components 폴더 업로드

---
+++ components/SidebarComponents/Sidebar1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Cities from '../../pages/Cities';
+
+function Sandan() {
+    return(
+        <div className="App">
+            <Cities />
+        </div>
+    )
+};
+
+export default Sandan;
