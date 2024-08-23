0b74085fb10c96b49f5daab8c38bc1f42e287387
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-23 14:48:26 +0900

    Feat: 라우터 내용 수정
    라우터 내용 수정

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,8 @@ import Login from './components/Login';
 import Home from './components/Home';
 import Map from "./pages/Map";
 import Exchange from "./pages/Exchange";
+import HomeDark from "./pages/NavBar/HomeColor/HomeDark";
+import HomeLight from "./pages/NavBar/HomeColor/HomeLight";  
 
 
 import Homepage from '../pages/Homepage';
@@ -21,6 +23,8 @@ const Router = () => {
       <Route path="/exchange" element={<Exchange />} />
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/map" element={<Map />} />
+      <Route path="/dark" element={<HomeDark />} />
+      <Route path="/light" element={<HomeLight />} />
     </Routes>
   
     
